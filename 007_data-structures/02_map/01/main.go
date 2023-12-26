0c9f8bea2d3aa7e1a52144eabeb3227c802e2b46
Hugo Orlando Gonzalez <122893651+gonzalez-atos@users.noreply.github.com>
2023-12-26 10:23:36 -0300

    :tada: update main.go

--- 007_data-structures/02_map/01/main.go
+++ 007_data-structures/02_map/01/main.go
@@ -14,8 +14,6 @@ func init() {
 
 func main() {
 
-	//sages := map[string]string{"India":"Gandhi", "America":"MLK", "Meditate":"Buddha", "Love":"Jesus", "Prophet":"Muhammad"}
-
 	sages := map[string]string{
 		"India":  "Docker",
 		"Google": "Go",
